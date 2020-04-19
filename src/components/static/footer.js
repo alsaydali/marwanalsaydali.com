2d1d09c693b5e68ded0b86b9c683d8d020067972
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-18 23:17:55 -0400

    worked in footer

--- src/components/static/footer.js
+++ src/components/static/footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import SocialMedia from "../reuseable/socialmedia"
 const Footer = (props) => (
   <footer>
+    <h4>{props.sitetitle}</h4>
     <SocialMedia class="footer__social" width="24px" />
   </footer>
 )
