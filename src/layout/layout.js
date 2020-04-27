940bc7d09a67f38c333e34f16693be79fcadc78d
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-27 10:09:24 -0400

    fixed the viewport

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -33,12 +33,15 @@ const Layout = ({ children }) => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>{siteTitle}</title>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="viewport-fit=cover width=device-width, initial-scale=1.0"
+        />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </Helmet>
       <Nav sitetitle={siteTitle} />
-      <main className="container mx-auto">{children}</main>
+      <main className="container mx-auto pt-10">{children}</main>
 
       <Footer sitetitle={siteTitle} />
     </>
