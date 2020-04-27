83da0a3ba2fefa586de7d90dfb740d503d8d7961
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-27 16:08:12 -0400

    styling the footer wirh tailwind

--- src/components/static/footer.js
+++ src/components/static/footer.js
@@ -1,12 +1,16 @@
 import React from "react"
 import SocialMedia from "../reuseable/socialmedia"
+
+const footerStyle = "container p-5 bg-gray-100 round"
+const footerText =
+  "text-center  xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl font-bold antialiased"
+const footerJob = `text-center text-pink-600 `
+const footerLoc = "flex justify-between w-24 m-auto mt-3"
 const Footer = (props) => (
-  <footer className="container  p-5 mb-5">
-    <h4 className="font-bold text-center  mb-5">{props.sitetitle}</h4>
-    <SocialMedia
-      class="flex items-center justify-between w-40 mx-auto"
-      width="20px"
-    />
+  <footer className={footerStyle}>
+    <h4 className={footerText}>{props.sitetitle}</h4>
+    <h5 className={footerJob}>{props.job}</h5>
+    <SocialMedia location={footerLoc} width="18px" />
   </footer>
 )
 
