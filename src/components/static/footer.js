146fef75d08f5b98addbd0bac8f4b1f74687737d
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-28 16:24:51 -0400

    small refactor

--- src/components/static/footer.js
+++ src/components/static/footer.js
@@ -5,7 +5,7 @@ const footerStyle = "container p-5 bg-gray-100 round"
 const footerText =
   "text-center  xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl font-bold antialiased"
 const footerJob = `text-center text-pink-600 `
-const footerLoc = "flex justify-between w-24 m-auto mt-3"
+const footerLoc = "flex justify-between w-24 m-auto mt-3 "
 const Footer = (props) => (
   <footer className={footerStyle}>
     <h4 className={footerText}>{props.sitetitle}</h4>
