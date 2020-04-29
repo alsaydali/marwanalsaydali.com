5300f67f46dc30bbc74ae7c4d1ec4ae36f5aa32a
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-29 17:24:39 -0400

    adding the feature image to the list

--- src/components/reuseable/postslist.js
+++ src/components/reuseable/postslist.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 
 const PostsList = (props) => {
   const data = useStaticQuery(
@@ -13,6 +14,13 @@ const PostsList = (props) => {
               frontmatter {
                 title
                 date(formatString: "DD MMMM, YYYY")
+                featuredImage {
+                  childImageSharp {
+                    fluid(maxWidth: 500) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
               }
               fields {
                 slug
@@ -25,16 +33,24 @@ const PostsList = (props) => {
     `
   )
   return (
-    <div className={props.class}>
+    <div className="">
       <h4>{`I Have ${data.allMarkdownRemark.totalCount} Posts here`}</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
+        <div key={node.id} className="bg-gray-100 p-3 mb-1 round">
           <Link to={node.fields.slug}>
-            <h3>
+            <div className="">
+              <Img
+                className=""
+                fluid={node.frontmatter.featuredImage.childImageSharp.fluid}
+              />
+            </div>
+            <h3 className="font-black">
               {node.frontmatter.title}
-              <span>— {node.frontmatter.date}</span>
+              <span className="ml-1 text-pink-600 font-bold">
+                — {node.frontmatter.date}
+              </span>
             </h3>
-            <p>{node.excerpt}</p>
+            <p className="antialiased">{node.excerpt}</p>
           </Link>
         </div>
       ))}
