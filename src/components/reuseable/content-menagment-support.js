9df4a034636a2e0d5a9c17f34bea6b217f4e0eb0
Marwan Alsaydali <alsaydali@icloud.com>
2020-05-06 19:08:47 -0400

    contents boxes updates

--- src/components/reuseable/content-menagment-support.js
+++ src/components/reuseable/content-menagment-support.js
@@ -2,13 +2,16 @@ import React from "react"
 import ContentsBox from "../../layout/contents-box"
 import Contents from "../../layout/contents"
 import CmsSupportText from "../contents/cms-support-text"
+import CmsSupportImg from "../contents/cms-support-img"
 const ContentManagementSupport = () => {
   return (
     <ContentsBox>
       <Contents order="order-last md:order-none">
         <CmsSupportText />
       </Contents>
-      <Contents>img</Contents>
+      <Contents>
+        <CmsSupportImg />
+      </Contents>
     </ContentsBox>
   )
 }
--- src/components/reuseable/online-store-support.js
+++ src/components/reuseable/online-store-support.js
@@ -2,10 +2,13 @@ import React from "react"
 import ContentsBox from "../../layout/contents-box"
 import Contents from "../../layout/contents"
 import CommraceSupportText from "../contents/commrace-support-text"
+import CommraceSupportImg from "../contents/commrace-support-img"
 const OnlineStoreSupport = () => {
   return (
     <ContentsBox>
-      <Contents>img</Contents>
+      <Contents>
+        <CommraceSupportImg />
+      </Contents>
       <Contents>
         <CommraceSupportText />
       </Contents>
