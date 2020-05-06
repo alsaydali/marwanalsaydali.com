b467787f9d0bbeb452904330c22257d147959225
Marwan Alsaydali <alsaydali@icloud.com>
2020-05-06 19:08:24 -0400

    Working on updating the the home page

--- src/components/static/header.js
+++ src/components/static/header.js
@@ -2,7 +2,10 @@ import React from "react"
 
 const Header = (props) => (
   <header className="">
-    <h2 className="object-auto">{props.title}</h2>
+    <h2 className="font-black  text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+      {props.title}
+    </h2>
+
     <p>{props.desc}</p>
   </header>
 )
